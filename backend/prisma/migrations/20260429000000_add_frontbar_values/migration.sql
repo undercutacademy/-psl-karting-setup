58457afdac93f14cde0521cbeb113fcf775e254e
Overcut48 <undercutacademy@gmail.com>
2026-04-29 07:39:17 -0500

    feat: extend FrontBar enum with Flat, Vertical, Gold, Chrome, Gray

---
+++ backend/prisma/migrations/20260429000000_add_frontbar_values/migration.sql
@@ -0,0 +1,6 @@
+-- AlterEnum
+ALTER TYPE "FrontBar" ADD VALUE 'Flat';
+ALTER TYPE "FrontBar" ADD VALUE 'Vertical';
+ALTER TYPE "FrontBar" ADD VALUE 'Gold';
+ALTER TYPE "FrontBar" ADD VALUE 'Chrome';
+ALTER TYPE "FrontBar" ADD VALUE 'Gray';
